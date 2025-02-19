230e4c3ce5fb8887264a2446c54ec5b4301372a7
facundo <facukinder@hotmail.com>
2025-02-19 18:32:28 -0300

    Hola Mundo inicial con React y TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>¡Hola Mundo, TaskManager está en marcha! 🚀</h1>
+      <p>Esto es una prueba inicial para verificar que todo funciona bien.</p>
+    </div>
+  );
+}
+
+export default App;
+
