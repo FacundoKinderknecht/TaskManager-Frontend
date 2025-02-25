63634a2091ccf8665435bc7764bfc58b0e2de27e
facundo <facukinder@hotmail.com>
2025-02-24 21:37:45 -0300

    arreglo path en el archivo json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/TaskManager-Frontend/',  //Esto es necesario para GitHub Pages
+  base: "/TaskManager-Frontend/", // Asegúrate de que esto es correcto
 });
